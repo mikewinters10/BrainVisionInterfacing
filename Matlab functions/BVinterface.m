68d79d64f340b0dcc6a330b7f4434a6b118e6853
Dwarfdeaths <dcoffm5261@gmail.com>
2015-11-20 13:46:36 -0500

    UDP data processing
    Receiving, unpacking OSC messages via UDP connection.

---
+++ Matlab functions/BVinterface.m
@@ -0,0 +1,176 @@
+function varargout = BVinterface(varargin)
+% BVINTERFACE MATLAB code for BVinterface.fig
+%      BVINTERFACE, by itself, creates a new BVINTERFACE or raises the existing
+%      singleton*.
+%
+%      H = BVINTERFACE returns the handle to a new BVINTERFACE or the handle to
+%      the existing singleton*.
+%
+%      BVINTERFACE('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in BVINTERFACE.M with the given input arguments.
+%
+%      BVINTERFACE('Property','Value',...) creates a new BVINTERFACE or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before BVinterface_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to BVinterface_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help BVinterface
+
+% Last Modified by GUIDE v2.5 20-Nov-2015 13:44:19
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @BVinterface_OpeningFcn, ...
+                   'gui_OutputFcn',  @BVinterface_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+end
+
+% --- Executes just before BVinterface is made visible.
+function BVinterface_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to BVinterface (see VARARGIN)
+
+%%% INITIAL VALUES
+
+handles.blocks = []; % Structure containing data blocks recieved via UDP
+handles.matrix = []; % 31xN matrix of channel voltages in time series
+handles.times = []; % Vector contianing the time of each sample in handles.matrix
+handles.record = false; % Variable for continuing to record
+handles.plot = false; % Variable for plotting
+handles.udp.connected = false;
+handles.udp.localPort = 5005; % Make sure this port matches the one in the python code
+
+% Choose default command line output for BVinterface
+handles.output = hObject;
+% Update handles structure
+guidata(hObject, handles);
+% UIWAIT makes BVinterface wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+end
+
+% --- Outputs from this function are returned to the command line.
+function varargout = BVinterface_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+end
+
+%%% Functions
+function connect(ho,h)
+h.udp.h = udp('localhost',h.udp.localPort,'LocalPort',h.udp.localPort,'InputBufferSize',131072,'InputDatagramPacketSize',65535); % Connect to port used by python program
+fopen(h.udp.h);
+h.udp.connected = true;
+set(ho,'String','Connected')
+guidata(ho,h);
+end
+
+function disconnect(ho,h,isButton)
+fclose(h.udp.h);
+clear h.udp.h
+h.udp.connected = false;
+if isButton
+set(ho,'String','Connect')
+end
+guidata(ho,h);
+end
+
+function record(hObject)
+handles = guidata(hObject);
+flushinput(handles.udp.h); % Flush buffer data for new recording
+if handles.udp.connected;
+while handles.record
+    if get(handles.udp.h, 'BytesAvailable') > 0
+        block = fread(handles.udp.h,1);
+        handles.blocks(end+1) = unpackBlock(block); % Do stuff to the block
+		nSamples = size(blocks(end).matrix,2);
+		handles.matrix(1:31,end+1:end+nSamples)=blocks(end).matrix;
+		
+		t = handles.blocks(end)-handles.blocks(1).timeStamp;
+		if t==0
+			handles.times(1:nSamples) = [0:nSamples-1]./nSamples; % Don't know block period yet
+		elseif length(handles.blocks)==2
+			T = handles.blocks(end).timeStamp-handles.blocks(end-1).timeStamp; % Period of block update
+			handles.times(end+1:end+nSamples) = [0:nSamples-1].*T./nSamples+t;
+			handles.times = handles.times*T; % Adjust the first vector to block period
+		else
+			T = handles.blocks(end).timeStamp-handles.blocks(end-1).timeStamp; % Period of block update
+			handles.times(end+1:end+nSamples) = [0:nSamples-1].*T./nSamples+t;
+		end
+		
+		if handles.plot
+			% Real time plotting?
+		end
+		
+    end
+    pause(.01)
+    handles = guidata(hObject);
+end
+end
+set(hObject,'Value',false)
+end
+
+
+%%% Callbacks
+
+function closereq(src,h)
+if h.udp.connected
+	disconnect(src,h,false)
+end
+delete(gcf)
+end
+
+function toggleConnect_Callback(hObject, ~, handles)
+if get(hObject,'Value')
+    connect(hObject,handles)
+else
+    disconnect(hObject,handles,true)
+end
+end
+
+function toggleRecord_Callback(hObject, ~, handles)
+handles.record = get(hObject,'Value');
+if handles.midiListen
+    record(hObject)
+end
+end
+
+
+function buttonSave_Callback(hObject, ~, handles)
+struc.matrix = handles.matrix;
+struc.times = handles.times;
+save(['data\' datestr(clock,'yy-mm-dd HH:MM:SS')],struc);
+end
+
+function buttonPlot_Callback(hObject, ~, handles)
+figure
+hold on
+for channel = 1:31
+	plot(handles.times,handles.matrix(channel,:),'k')
+end
+end
--- Matlab functions/unpackBlock.m
+++ Matlab functions/unpackBlock.m
@@ -2,19 +2,41 @@ function [struc] = unpackBlock(block)
 % Takes a raw (UDP) data block sent by python OSC from BrainVision and puts the data into a structure containing
 % - The address
 % - The contents list
-% - The time stamp
-% - The voltages, divided into 32 channels
+% - The time stamp, in miliseconds (converted from H:M:S string)
+% - The voltage matrix, divided into 31 channels and N samples
 
+numChannels = 31;
 
-% TODO:
-% Read until four zeros - this is the address string (mostly uselss for now)
-% Read again until four zeros - this is the contents list
-% Read until one zero - this is the timestamp (in string format)
-% Read (total floats/31) floats of 4 byte precision, big-endian
-	% Make into a vector - this is the data for one channel
-	% Repeat for the remaining channels (total of 31 channels)
+sblock = char(block);
+[struc.address,sblock] = strtok(sblock,char(0)); % Read until four zeros - this is the address string
+sblock=sblock(5:end); % Removes unused zeros
 
-% ? Convert timestamp to time format or simply miliseconds
+[struc.contents,sblock] = strtok(sblock,char(0)); % Read again until four zeros - this is the contents list
+sblock=sblock(5:end); % Removes unused zeros
+
+numSamples=sum(struc.contents=='f')/31;
+
+[tstamp,sblock] = strtok(sblock,char(0)); % Read until one zero - this is the timestamp (in string format)
+sblock=sblock(2:end); % Removes unused zero
+
+[hour,rest] = strtok(tstamp',':');
+[minute,rest] = strtok(rest(2:end),':');
+[second] = rest(2:end);
+struc.timeStamp = str2double(second)+str2double(minute)*60+str2double(hour)*60*60;
+
+channelData = uint8(sblock+1-1); % convert remaining data (4-byte floats) back to number format
+
+struc.address = struc.address';
+struc.contents = struc.contents';
+
+temp = reshape(channelData,[4,numSamples,numel(channelData)/(4*numSamples)]);
+struc.matrix = single(zeros(numChannels,numSamples));
+for channel = 1:numChannels
+	for sample = 1:numSamples
+		struc.matrix(channel,sample) = typecast(fliplr(temp(:,sample,channel)'),'single');
+	end
+end
+
 % ? In another function, make a structure vector contianing each unpacked block structure
 %   and also populate a time-series matrix with the floats and delta-times
 
